d69b5c5408fd80abd070430509ff7efcdaba59ff
Nico_Oliveira39 <98227758+Stevesabao01@users.noreply.github.com>
2023-08-25 06:39:52 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+alert("Texto Alternativo.")
+
+// boolean
+var VOuF = false;
+console.log(typeof(VOuF));
+
+// Number
+var NumeroQualquer = 3;
+console.log(typeof(NumeroQualquer));
+
+// String
+var nome = 'Fulano de Tal'
+console.log(typeof(nome));
+
+// Function
+var funcao = function() {}
+console.log(typeof(funcao));
+
